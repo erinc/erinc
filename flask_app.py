5daa5a7881436c8fb5dd1d9aa95f375a673f7d51
Erinc <erinc@erinc.ca>
2016-03-03 09:57:31 -0800

    Products page

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/products/')
+def products():
+    return render_template('products.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
