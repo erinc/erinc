e8240912b5f17ae481e0787abe6462215f7cb3f8
Erinc M <erinc@erinc.ca>
2016-03-08 13:41:08 -0800

    Added about page

--- flask_app.py
+++ flask_app.py
@@ -12,6 +12,10 @@ def index():
 def products():
     return render_template('products.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)    
